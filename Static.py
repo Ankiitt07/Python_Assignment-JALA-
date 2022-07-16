cbb8cf1f5a4934b653ede7faf403659456f20ab3
Ankiitt07 <100189882+Ankiitt07@users.noreply.github.com>
2022-07-16 18:51:24 +0530

    Add files via upload

---
+++ Static.py
@@ -0,0 +1,21 @@
+# Define a static variable and access that through a class.
+
+class Python:
+    # Access through class
+    staticVariable = 9
+
+
+print(Python.staticVariable)
+
+# Access through an instance
+instance = Python()
+print(instance.staticVariable)  # Gives 12
+
+# Change within an instance
+instance.staticVariable = 15
+print(instance.staticVariable)  # Gives 15
+print(Python.staticVariable)  # Gives 12
+
+# Change within a class
+Python.staticVariable = 12
+print(Python.staticVariable)  # Gives 12
